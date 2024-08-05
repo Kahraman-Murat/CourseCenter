00b91528ece86bb35bf6784f19c39c35966f1fe1
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-08-05 19:21:51 +0200

    Implemented SubscriberDtos and SubscriberMapping / Prepared SubscribersController

---
+++ CourseCenter.API/Controllers/SubscribersController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CourseCenter.Business.Abstract;
+using CourseCenter.DTO.DTOs.SocialMediaDtos;
+using CourseCenter.DTO.DTOs.SubscriberDtos;
+using CourseCenter.Entity.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscribersController(IGenericService<Subscriber> _subscriberService, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var datas = _subscriberService.TGetList();
+            return Ok(datas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var data = _subscriberService.TGetById(id);
+            return Ok(data);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _subscriberService.TDelete(id);
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateSubscriberDto createSubscriberDto)
+        {
+            var newData = _mapper.Map<Subscriber>(createSubscriberDto);
+            _subscriberService.TCreate(newData);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateSubscriberDto updateSubscriberDto)
+        {
+            var newData = _mapper.Map<Subscriber>(updateSubscriberDto);
+            _subscriberService.TUpdate(newData);
+            return Ok();
+        }
+    }
+}
---
+++ CourseCenter.API/Mapping/SubscriberMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CourseCenter.DTO.DTOs.SubscriberDtos;
+using CourseCenter.Entity.Entities;
+
+namespace CourseCenter.API.Mapping
+{
+    public class SubscriberMapping : Profile
+    {
+        public SubscriberMapping()
+        {
+            CreateMap<CreateSubscriberDto, Subscriber>().ReverseMap();
+            CreateMap<UpdateSubscriberDto, Subscriber>().ReverseMap();
+            CreateMap<ResultSubscriberDto, Subscriber>().ReverseMap();
+        }
+    }
+}
