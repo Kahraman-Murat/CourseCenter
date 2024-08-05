043f4f36aacdc53f7f1dbcb0af239fd354bb8b84
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-08-05 18:14:06 +0200

    Implemented ContactDtos and ContactMapping. Prepared ContactsController

---
+++ CourseCenter.API/Controllers/ContactsController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using CourseCenter.Business.Abstract;
+using CourseCenter.DTO.DTOs.ContactDtos;
+using CourseCenter.Entity.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController(IGenericService<Contact> _contactServıce, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var datas = _contactServıce.TGetList();
+            return Ok(datas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var data = _contactServıce.TGetById(id);
+            return Ok(data);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _contactServıce.TDelete(id);
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateContactDto createContactDto)
+        {
+            var newData = _mapper.Map<Contact>(createContactDto);
+            _contactServıce.TCreate(newData);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateContactDto updateContactDto)
+        {
+            var newData = _mapper.Map<Contact>(updateContactDto);
+            _contactServıce.TUpdate(newData);
+            return Ok();
+        }
+    }
+}
---
+++ CourseCenter.API/Mapping/ContactMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CourseCenter.DTO.DTOs.ContactDtos;
+using CourseCenter.Entity.Entities;
+
+namespace CourseCenter.API.Mapping
+{
+    public class ContactMapping : Profile
+    {
+        public ContactMapping()
+        {
+            CreateMap<CreateContactDto, Contact>().ReverseMap();
+            CreateMap<UpdateContactDto, Contact>().ReverseMap();
+            CreateMap<ResultContactDto, Contact>().ReverseMap();
+        }
+    }
+}
+
