16441a59e7921e3bf8df1a6c44031e602a5ca152
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-08-05 19:18:46 +0200

    Implemented MessageDtos and MessageMapping / Prepared MessagesController

---
+++ CourseCenter.API/Controllers/MessagesController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CourseCenter.Business.Abstract;
+using CourseCenter.DTO.DTOs.BannerDtos;
+using CourseCenter.DTO.DTOs.MessageDtos;
+using CourseCenter.Entity.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController(IGenericService<Message> _messageService, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var datas = _messageService.TGetList();
+            return Ok(datas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var data = _messageService.TGetById(id);
+            return Ok(data);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _messageService.TDelete(id);
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateMessageDto createMessageDto)
+        {
+            var newData = _mapper.Map<Message>(createMessageDto);
+            _messageService.TCreate(newData);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateMessageDto updateMessageDto)
+        {
+            var newData = _mapper.Map<Message>(updateMessageDto);
+            _messageService.TUpdate(newData);
+            return Ok();
+        }
+    }
+}
---
+++ CourseCenter.API/Mapping/MessageMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CourseCenter.DTO.DTOs.MessageDtos;
+using CourseCenter.Entity.Entities;
+
+namespace CourseCenter.API.Mapping
+{
+    public class MessageMapping : Profile
+    {
+        public MessageMapping()
+        {
+            CreateMap<CreateMessageDto, Message>().ReverseMap();
+            CreateMap<UpdateMessageDto, Message>().ReverseMap();
+            CreateMap<ResultMessageDto, Message>().ReverseMap();
+        }
+    }
+}
