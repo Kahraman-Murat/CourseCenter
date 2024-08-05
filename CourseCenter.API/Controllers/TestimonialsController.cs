74096ff07548d8bd6784174ecd49121b00631aa4
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-08-05 19:23:23 +0200

    Implemented TestimonialDtos and TestimonialMapping / Prepared TestimonialsController

---
+++ CourseCenter.API/Controllers/TestimonialsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CourseCenter.Business.Abstract;
+using CourseCenter.DTO.DTOs.SubscriberDtos;
+using CourseCenter.DTO.DTOs.TestimonialDtos;
+using CourseCenter.Entity.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController(IGenericService<Testimonial> _testimonialService, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var datas = _testimonialService.TGetList();
+            return Ok(datas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var data = _testimonialService.TGetById(id);
+            return Ok(data);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _testimonialService.TDelete(id);
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateTestimonialDto createTestimonialDto)
+        {
+            var newData = _mapper.Map<Testimonial>(createTestimonialDto);
+            _testimonialService.TCreate(newData);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateTestimonialDto updateTestimonialDto)
+        {
+            var newData = _mapper.Map<Testimonial>(updateTestimonialDto);
+            _testimonialService.TUpdate(newData);
+            return Ok();
+        }
+    }
+}
---
+++ CourseCenter.API/Mapping/TestimonialMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CourseCenter.DTO.DTOs.TestimonialDtos;
+using CourseCenter.Entity.Entities;
+
+namespace CourseCenter.API.Mapping
+{
+    public class TestimonialMapping : Profile
+    {
+        public TestimonialMapping()
+        {
+            CreateMap<CreateTestimonialDto, Testimonial>().ReverseMap();
+            CreateMap<UpdateTestimonialDto, Testimonial>().ReverseMap();
+            CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();
+        }
+    }
+}
