4d6c78127bd61b0ba482819513d5442fed255139
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-08-07 14:42:19 +0200

    Added HttpClientInstance class in Helpers directory  for Consume.

---
+++ CourseCenter.WebUI/Helpers/HttpClientInstance.cs
@@ -0,0 +1,17 @@
+﻿namespace CourseCenter.WebUI.Helpers
+{
+    public static class HttpClientInstance
+    {
+
+        public static HttpClient CreateClient()
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://localhost:7092/api/");
+            
+            return client;
+        } 
+
+
+    }
+}
