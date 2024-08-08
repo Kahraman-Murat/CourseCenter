3fff7a8077af2c34370a86d24175ad6b6f83066a
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-08-08 22:16:10 +0200

    Added JsonSerializerOptions for Controllers and Registered new files for  BlogCatoryName

--- CourseCenter.API/Program.cs
+++ CourseCenter.API/Program.cs
@@ -1,10 +1,12 @@
 using CourseCenter.Business.Abstract;
 using CourseCenter.Business.Concrete;
 using CourseCenter.DataAccess.Abstract;
+using CourseCenter.DataAccess.Concrete;
 using CourseCenter.DataAccess.Context;
 using CourseCenter.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,13 +19,18 @@ builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
 
+builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+builder.Services.AddScoped<IBlogService, BlogManager>();
+
 // Add DbContext
 builder.Services.AddDbContext<CourseCenterContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"));
 });
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
