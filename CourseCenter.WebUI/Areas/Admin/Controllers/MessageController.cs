33f71259e4fc063aeca870ffa089e6234456f4c8
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-08-12 16:24:30 +0200

    Added MessageController and Views

---
+++ CourseCenter.WebUI/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using CourseCenter.WebUI.DTOs.MessageDtos;
+using CourseCenter.WebUI.Helpers;
+using CourseCenter.WebUI.Validators;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class MessageController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
+        {
+            var datas = await _client.GetFromJsonAsync<List<ResultMessageDto>>("Messages");
+            return View(datas);
+        }
+
+        public async Task<IActionResult> DeleteMessage(int id)
+        {
+            await _client.DeleteAsync($"Messages/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MessageDetail(int id)
+        {
+            var data = await _client.GetFromJsonAsync<ResultMessageDto>($"Messages/{id}");
+            return View(data);
+        }
+                
+    }
+}
---
+++ CourseCenter.WebUI/Validators/CreateMessageValidator.cs
@@ -0,0 +1,19 @@
+﻿using CourseCenter.WebUI.DTOs.BannerDtos;
+using CourseCenter.WebUI.DTOs.MessageDtos;
+using FluentValidation;
+
+namespace CourseCenter.WebUI.Validators
+{
+    public class CreateMessageValidator : AbstractValidator<CreateMessageDto>
+    {
+        public CreateMessageValidator()
+        {
+            RuleFor(x=>x.Name).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Gecerli bir Email Adresi girin.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+            RuleFor(x => x.Content).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+        }
+    }
+}
