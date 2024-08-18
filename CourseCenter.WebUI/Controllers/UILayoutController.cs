00f620696fb26100fa280cc7693d8bfbc6467cea
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-08-18 16:04:54 +0200

    Added UILayoutController and Views in WebUI

---
+++ CourseCenter.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult layout()
+        {
+            return View();
+        }
+    }
+}
