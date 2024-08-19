0551d535f6c31ae233c453d00ea76c3933d7bdba
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-08-19 18:50:56 +0200

    Added Banner, CourseCategory, Course ViewComponents for Home Index View

---
+++ CourseCenter.WebUI/ViewComponents/Home/_HomeCourseCategoryComponent.cs
@@ -0,0 +1,16 @@
+﻿using CourseCenter.WebUI.DTOs.CourseCategoryDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Home
+{
+    public class _HomeCourseCategoryComponent : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var datas = await _client.GetFromJsonAsync<List<ResultCourseCategoryDto>>("courseCategories/GetActiveCategories");
+            return View(datas);
+        }
+    }
+}
---
+++ CourseCenter.WebUI/ViewComponents/Home/_HomeCourseComponent.cs
@@ -0,0 +1,17 @@
+﻿using CourseCenter.WebUI.DTOs.CourseDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Home
+{
+    public class _HomeCourseComponent : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var datas = await _client.GetFromJsonAsync<List<ResultCourseDto>>("courses/GetActiveCourses");
+            return View(datas);
+        }
+    }
+}
