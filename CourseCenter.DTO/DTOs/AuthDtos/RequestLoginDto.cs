d01ca99a7fc8ec6833e537b2499da7be4860895e
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-09-03 23:06:26 +0200

    Added RequestLoginDto and ResponseLoginDto

---
+++ CourseCenter.DTO/DTOs/AuthDtos/RequestLoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.DTO.DTOs.AuthDtos
+{
+    public class RequestLoginDto
+    {
+        [DefaultValue("muratkahraman.de@gmail.com")]
+        public string Email { get; set; }
+        [DefaultValue("1234")]
+        public string Password { get; set; }
+    }
+}
