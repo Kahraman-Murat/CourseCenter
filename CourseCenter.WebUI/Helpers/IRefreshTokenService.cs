e99cadd29f56c1d82a801084ff9ffa5816fa7436
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-09-13 23:32:31 +0200

    feat(WebUI): add RefreshTokenService to refresh the accesstoken - Added RefreshTokenService to refresh the accesstoken if a valid refresh token is provided

---
+++ CourseCenter.WebUI/Helpers/IRefreshTokenService.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseCenter.WebUI.Helpers
+{
+    public interface IRefreshTokenService
+    {
+        Task<bool> RefreshTokensAsync();
+        void RemoveTokensCookies();
+        void SaveTokensCookies(string accessToken, string refreshToken);
+        string GetAccessToken();
+    }
+}
---
+++ CourseCenter.WebUI/Helpers/RefreshTokenService.cs
@@ -0,0 +1,85 @@
+﻿
+using CourseCenter.WebUI.DTOs.AuthDtos;
+
+namespace CourseCenter.WebUI.Helpers
+{
+    public class RefreshTokenService(IHttpContextAccessor _httpContextAccessor, IServiceProvider _serviceProvider) : IRefreshTokenService
+    {
+        public string GetAccessToken()
+        {
+            return _httpContextAccessor.HttpContext.Items["AccessToken"] as string
+               ?? _httpContextAccessor.HttpContext.Request.Cookies["AccessToken"];
+        }
+
+        public async Task<bool> RefreshTokensAsync()
+        {
+            RequestTokenDto requestTokenDto = new()
+            {
+                AccessToken = _httpContextAccessor.HttpContext.Request.Cookies["AccessToken"] ?? "",
+                RefreshToken = _httpContextAccessor.HttpContext.Request.Cookies["RefreshToken"]
+            };
+
+            if (string.IsNullOrEmpty(requestTokenDto.RefreshToken))
+                return false;
+
+            var httpClientService = _serviceProvider.GetService<IHttpClientService>();
+            ResponseTokenDto tokenResponse = await httpClientService.SendRequestAsync<RequestTokenDto, ResponseTokenDto>(HttpMethod.Post, "auths/refreshtokencheck", requestTokenDto);
+
+            if (tokenResponse == null)
+                return false;
+
+            RemoveTokensCookies();
+            SaveTokensCookies(tokenResponse.AccessToken, tokenResponse.RefreshToken);
+
+            return true;
+        }
+
+        public void RemoveTokensCookies()
+        {
+            _httpContextAccessor.HttpContext.Response.Cookies.Delete("AccessToken", new CookieOptions
+            {
+                HttpOnly = true,                           // JavaScript ile erişilemez
+                Secure = true,                             // HTTPS zorunlu
+                Expires = DateTime.UtcNow.AddDays(-1),     // Çerezin süresi geçmiş bir tarihe ayarlanır
+                SameSite = SameSiteMode.None,              // CSRF korunması için // Strict,Lax,None 
+                Path = "/",                                // Çerezin tüm yollar için geçerli olması
+                //Domain = "localhost:7102/"               // Çerezin domaini (istenirse)
+            });
+
+            _httpContextAccessor.HttpContext.Response.Cookies.Delete("RefreshToken", new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                Expires = DateTime.UtcNow.AddDays(-1),
+                SameSite = SameSiteMode.None,
+                Path = "/",
+                //Domain = "localhost:7102/"
+            });
+        }
+
+        public void SaveTokensCookies(string accessToken, string refreshToken)
+        {
+            _httpContextAccessor.HttpContext.Response.Cookies.Append("AccessToken", accessToken, new CookieOptions
+            {
+                HttpOnly = true,                           // JavaScript ile erişilemez
+                Secure = true,                             // HTTPS zorunlu
+                Expires = DateTime.UtcNow.AddMinutes(15),  // Token süresi
+                SameSite = SameSiteMode.None,              // CSRF korunması için // Strict,Lax,None 
+                Path = "/",                                // Çerezin tüm yollar için geçerli olması
+                //Domain = "localhost:7102/"               // Çerezin domaini (istenirse)
+            });
+            _httpContextAccessor.HttpContext.Items["AccessToken"] = accessToken;
+
+            _httpContextAccessor.HttpContext.Response.Cookies.Append("RefreshToken", refreshToken, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                Expires = DateTime.UtcNow.AddDays(7),
+                SameSite = SameSiteMode.None,
+                Path = "/",
+                //Domain = "localhost:7102/"
+            });
+            _httpContextAccessor.HttpContext.Items["RefreshToken"] = refreshToken;
+        }
+    }
+}
