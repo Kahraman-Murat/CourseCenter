a793c94bd51a60d9cd14eb6f258cb25d9d2746cd
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-09-26 17:44:39 +0200

    refactor(WebUI): update BlogCategoryController in Admin area to use HttpClientService - Refactored `BlogCategoryController` in the Admin area to utilize `HttpClientService` for handling HTTP requests.

--- CourseCenter.WebUI/Areas/Admin/Controllers/BlogCategoryController.cs
+++ CourseCenter.WebUI/Areas/Admin/Controllers/BlogCategoryController.cs
@@ -1,33 +1,20 @@
 ﻿using CourseCenter.WebUI.DTOs.BlogCategoryDtos;
 using CourseCenter.WebUI.Helpers;
 using CourseCenter.WebUI.Validators;
-using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CourseCenter.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Route("[area]/[controller]/[action]/{id?}")]
-    public class BlogCategoryController : Controller
+    public class BlogCategoryController(IHttpClientService _httpClientService) : Controller
     {
-        private readonly HttpClient _client = HttpClientInstance.CreateClient();
-        public async Task<IActionResult> Index()
-        {
-            var datas = await _client.GetFromJsonAsync<List<ResultBlogCategoryDto>>("BlogCategories");
-            return View(datas);
-        }
-
-        public async Task<IActionResult> DeleteBlogCategory(int id)
-        {
-            await _client.DeleteAsync($"BlogCategories/{id}");
-            return RedirectToAction(nameof(Index));
-        }
+        [HttpGet]
+        public async Task<IActionResult> Index() =>
+            View(await _httpClientService.SendRequestAsync<string, List<ResultBlogCategoryDto>>(HttpMethod.Get, "BlogCategories", default));
 
         [HttpGet]
-        public async Task<IActionResult> CreateBlogCategory()
-        {
-            return View();
-        }
+        public async Task<IActionResult> CreateBlogCategory() => View();
 
         [HttpPost]
         public async Task<IActionResult> CreateBlogCategory(CreateBlogCategoryDto createBlogCategoryDto)
@@ -38,23 +25,19 @@ namespace CourseCenter.WebUI.Areas.Admin.Controllers
             {
                 ModelState.Clear();
                 foreach (var x in result.Errors)
-                {
                     ModelState.AddModelError(x.PropertyName, x.ErrorMessage);
-                }
 
                 return View(createBlogCategoryDto);
             }
 
-            await _client.PostAsJsonAsync("BlogCategories", createBlogCategoryDto);
+            await _httpClientService.SendRequestAsync<CreateBlogCategoryDto, string>(HttpMethod.Post, "BlogCategories", createBlogCategoryDto);
+
             return RedirectToAction(nameof(Index));
         }
 
         [HttpGet]
-        public async Task<IActionResult> UpdateBlogCategory(int id)
-        {
-            var datas = await _client.GetFromJsonAsync<UpdateBlogCategoryDto>($"BlogCategories/{id}");
-            return View(datas);
-        }
+        public async Task<IActionResult> UpdateBlogCategory(int id) =>
+            View(await _httpClientService.SendRequestAsync<string, UpdateBlogCategoryDto>(HttpMethod.Get, $"BlogCategories/{id}", default));
 
         [HttpPost]
         public async Task<IActionResult> UpdateBlogCategory(UpdateBlogCategoryDto updateBlogCategoryDto)
@@ -65,14 +48,20 @@ namespace CourseCenter.WebUI.Areas.Admin.Controllers
             {
                 ModelState.Clear();
                 foreach (var x in result.Errors)
-                {
                     ModelState.AddModelError(x.PropertyName, x.ErrorMessage);
-                }
 
                 return View(updateBlogCategoryDto);
             }
 
-            await _client.PutAsJsonAsync("BlogCategories", updateBlogCategoryDto);
+            await _httpClientService.SendRequestAsync<UpdateBlogCategoryDto, string>(HttpMethod.Put, "BlogCategories", updateBlogCategoryDto);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> DeleteBlogCategory(int id)
+        {
+            await _httpClientService.SendRequestAsync<string, string>(HttpMethod.Delete, $"BlogCategories/{id}", default);
+
             return RedirectToAction(nameof(Index));
         }
     }
