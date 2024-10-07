d7320635c9d1c0d7d112894f7956c5c8b1dbef11
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-10-07 11:43:27 +0200

    feat(WebUI): add new DTO files to UserDtos as part of restructuring
    - Added several new DTO files to `UserDtos` to support the recent code restructuring and improvements.

---
+++ CourseCenter.WebUI/DTOs/UserDtos/AssignUserRolesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseCenter.WebUI.DTOs.UserDtos
+{
+    public class AssignUserRolesDto
+    {
+        public int UserId { get; set; }
+        public List<RoleStateDto> RoleStateDtos { get; set; } = new List<RoleStateDto>();
+    }
+
+}
---
+++ CourseCenter.WebUI/DTOs/UserDtos/ResultUserRolesDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.WebUI.DTOs.UserDtos
+{
+    public class ResultUserRolesDto
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public List<RoleStateDto> RoleStateDtos { get; set; } = new List<RoleStateDto>();
+    }
+
+    
+}
