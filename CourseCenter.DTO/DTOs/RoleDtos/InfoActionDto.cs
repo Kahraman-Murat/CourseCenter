ad06546f18819b23c851f37b490a8e9f3894ac72
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-10-10 11:40:19 +0200

    feat(DTO): create DTO for roles defined in controllers and actions
    - Added DTO to represent roles defined in controllers and actions for better role management.

---
+++ CourseCenter.DTO/DTOs/RoleDtos/InfoActionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.DTO.DTOs.RoleDtos
+{
+    public class InfoActionDto
+    {
+        public string ActionName { get; set; }
+        public List<string> AuthorizeRoles { get; set; }
+    }
+}
---
+++ CourseCenter.DTO/DTOs/RoleDtos/InfoControllerDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.DTO.DTOs.RoleDtos
+{
+    public class InfoControllerDto
+    {
+        public string ControllerName { get; set; }
+        public List<InfoActionDto> Actions { get; set; }
+    }
+}
