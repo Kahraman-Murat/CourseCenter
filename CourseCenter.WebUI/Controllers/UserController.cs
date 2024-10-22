8d62587d1fe3c8fea8dea0c7ce82eac0e3a426c2
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-10-22 20:43:52 +0200

    refactor(WebUI): update UserController to use HttpClientService - Refactored `UserController` to utilize `HttpClientService` for handling HTTP requests.

--- CourseCenter.WebUI/Controllers/UserController.cs
+++ CourseCenter.WebUI/Controllers/UserController.cs
@@ -1,21 +1,19 @@
-﻿using CourseCenter.WebUI.DTOs.BannerDtos;
+﻿//using AutoMapper;
+using CourseCenter.WebUI.DTOs.AuthDtos;
+using CourseCenter.WebUI.DTOs.BannerDtos;
 using CourseCenter.WebUI.DTOs.UserDtos;
 using CourseCenter.WebUI.Helpers;
 using CourseCenter.WebUI.Validators;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace CourseCenter.WebUI.Controllers
 {
-    public class UserController : Controller
+    public class UserController(IHttpClientService _httpClientService) : Controller
     {
-        private readonly HttpClient _client = HttpClientInstance.CreateClient();
-
         [HttpGet]
-        public IActionResult Signup()
-        {
-            return View();
-        }
+        public IActionResult Signup() => View();
 
         [HttpPost]
         public async Task<IActionResult> Signup(CreateUserDto createUserDto)
@@ -26,15 +24,14 @@ namespace CourseCenter.WebUI.Controllers
             {
                 ModelState.Clear();
                 foreach (var x in result.Errors)
-                {
                     ModelState.AddModelError(x.PropertyName, x.ErrorMessage);
-                }
 
                 return View(createUserDto);
             }
 
-            var response = await _client.PostAsJsonAsync("users/Create", createUserDto);
-            return RedirectToAction("Index","Home");
+            await _httpClientService.SendRequestAsync<CreateUserDto, object>(HttpMethod.Post, "users/Create", createUserDto);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
