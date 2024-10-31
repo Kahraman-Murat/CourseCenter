604e1b97dff74cb6e3bdfb3f47d67bf72cefd59f
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-10-31 21:32:36 +0100

    feat(WebUI): add RequestTokenDto, ResponseTokenDto, and RequestRevokeDto
    - Created `RequestTokenDto` and `ResponseTokenDto` for token request and response handling.
    - Added `RequestRevokeDto` to manage token revocation requests.

---
+++ CourseCenter.WebUI/DTOs/AuthDtos/RequestRevokeDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.WebUI.DTOs.AuthDtos
+{
+    public class RequestRevokeDto
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ CourseCenter.WebUI/DTOs/AuthDtos/RequestTokenDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.WebUI.DTOs.AuthDtos
+{
+    public class RequestTokenDto
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+
+    }
+}
