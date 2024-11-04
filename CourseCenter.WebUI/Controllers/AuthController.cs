c858a44541d13934669b8c11210deefb55c6af8f
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-11-04 19:54:32 +0100

    feat(WebUI): create AuthController with Login action
    - Implemented `AuthController` to handle authentication requests.
    - Added `Login` action for user authentication.

---
+++ CourseCenter.WebUI/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using CourseCenter.WebUI.DTOs.AuthDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CourseCenter.WebUI.Controllers
+{
+    public class AuthController(IHttpClientService _httpClientService, IRefreshTokenService _refreshTokenService) : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null) => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(RequestLoginDto requestLoginDto, string? returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseLoginDto tokenResponse = await _httpClientService.SendRequestAsync<RequestLoginDto, ResponseLoginDto>(HttpMethod.Post, "auths/login", requestLoginDto);
+
+                if (tokenResponse != null)
+                {                    
+                    _refreshTokenService.RemoveTokensCookies();
+                    _refreshTokenService.SaveTokensCookies(tokenResponse.AccessToken, tokenResponse.RefreshToken);
+                                        
+                    if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                        return Redirect(returnUrl);
+                    else
+                        return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "Gecersiz login denemesi.");
+            }
+            
+            return View(requestLoginDto);
+        }
+    }
+}
