098cab0233e425815d94259903db74f6363bc6e4
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-11-13 23:43:24 +0100

    feat(API): add role-based Authorize attributes in ContactsController for role operation testing
    - Implemented different role-specific Authorize attributes in ContactsController.
    - Conducted code validation for role-based access control within controller actions.

--- CourseCenter.API/Controllers/ContactsController.cs
+++ CourseCenter.API/Controllers/ContactsController.cs
@@ -10,9 +10,10 @@ namespace CourseCenter.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    
     public class ContactsController(IGenericService<Contact> _contactServıce, IMapper _mapper) : ControllerBase
     {
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public IActionResult Get()
         {
@@ -20,20 +21,21 @@ namespace CourseCenter.API.Controllers
             return Ok(datas);
         }
 
+        [Authorize(Roles = "Kimki")]
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
             var data = _contactServıce.TGetById(id);
             return Ok(data);
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
             _contactServıce.TDelete(id);
             return Ok();
         }
-
+        [Authorize(Roles = "Teacher")]
         [HttpPost]
         public IActionResult Create(CreateContactDto createContactDto)
         {
