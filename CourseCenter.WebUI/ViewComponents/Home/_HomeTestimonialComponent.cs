235c08dd4415851adbd94109167bfe1f1baf2db0
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-11-21 21:43:11 +0100

    feat(WebUI): add _HomeTestimonialComponent file as ViewComponent

---
+++ CourseCenter.WebUI/ViewComponents/Home/_HomeTestimonialComponent.cs
@@ -0,0 +1,12 @@
+﻿using CourseCenter.WebUI.DTOs.TestimonialDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Home
+{
+    public class _HomeTestimonialComponent(IHttpClientService _httpClientService) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() =>
+            View(await _httpClientService.SendRequestAsync<string, List<ResultTestimonialDto>>(HttpMethod.Get, "testimonials", default));
+    }
+}
