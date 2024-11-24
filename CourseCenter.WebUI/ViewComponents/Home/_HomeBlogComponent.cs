21343ef8d3b7d8d0ac07cd30d6e67d57bace8ce8
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-11-24 15:00:34 +0100

    feat(WebUI): add _HomeBlogComponent file as ViewComponent

---
+++ CourseCenter.WebUI/ViewComponents/Home/_HomeBlogComponent.cs
@@ -0,0 +1,13 @@
+﻿
+using CourseCenter.WebUI.DTOs.BlogDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Home
+{
+    public class _HomeBlogComponent(IHttpClientService _httpClientService) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() =>
+            View(await _httpClientService.SendRequestAsync<string, List<ResultBlogDto>>(HttpMethod.Get, "blogs/GetLast4Blogs", default));
+    }
+}
