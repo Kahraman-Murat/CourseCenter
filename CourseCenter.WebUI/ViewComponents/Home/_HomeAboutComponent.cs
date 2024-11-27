79734c3ab08a68bfed7538a49011dcf5ae7e4d7a
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-11-27 11:28:18 +0100

    feat(WebUI): add _HomeAboutComponent file as ViewComponent

---
+++ CourseCenter.WebUI/ViewComponents/Home/_HomeAboutComponent.cs
@@ -0,0 +1,12 @@
+﻿using CourseCenter.WebUI.DTOs.AboutDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Home
+{
+    public class _HomeAboutComponent(IHttpClientService _httpClientService) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() =>
+            View(await _httpClientService.SendRequestAsync<string, List<ResultAboutDto>>(HttpMethod.Get, "abouts", default));
+    }
+}
