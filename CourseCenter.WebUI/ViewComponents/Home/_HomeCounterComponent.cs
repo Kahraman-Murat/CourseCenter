6a94ef288b3bd52cf690c2b591bf32352a3855f0
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-12-06 17:45:05 +0100

    feat(WebUI): add __HomeCounterComponent file as ViewComponent

---
+++ CourseCenter.WebUI/ViewComponents/Home/_HomeCounterComponent.cs
@@ -0,0 +1,22 @@
+﻿using CourseCenter.WebUI.DTOs.BannerDtos;
+using CourseCenter.WebUI.DTOs.BlogDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Home
+{
+    public class _HomeCounterComponent(IHttpClientService _httpClientService) : ViewComponent
+    {
+        //public async Task<IViewComponentResult> InvokeAsync() => 
+        //    View(await _httpClientService.SendRequestAsync<string, List<ResultBannerDto>>(HttpMethod.Get, "banners", default));
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            
+            ViewBag.courseCategoryCount = await _httpClientService.SendRequestAsync<string, int>(HttpMethod.Get, "courseCategories/GetCategoryCount", default);
+            ViewBag.courseCount = await _httpClientService.SendRequestAsync<string, int>(HttpMethod.Get, "courses/GetCourseCount", default);
+            ViewBag.blogCount = await _httpClientService.SendRequestAsync<string, int>(HttpMethod.Get, "blogs/GetBlogCount", default);
+            ViewBag.teacherCount = await _httpClientService.SendRequestAsync<string, int>(HttpMethod.Get, "users/GetUserCountInRoles/Teacher", default);
+            return View();
+        }
+    }
+}
