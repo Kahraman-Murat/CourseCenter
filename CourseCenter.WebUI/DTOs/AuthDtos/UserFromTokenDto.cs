991ca4bfb543f69b968ee7260b656b49f31b304e
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-12-10 20:10:48 +0100

    feat(WebUI): create UserFromTokenDto for extracting payload values from JWT AccessToken
    - Added `UserFromTokenDto` in the WebUI project to parse and handle payload data from JWT AccessToken.

---
+++ CourseCenter.WebUI/DTOs/AuthDtos/UserFromTokenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseCenter.WebUI.DTOs.AuthDtos
+{
+    public class UserFromTokenDto
+    {
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
