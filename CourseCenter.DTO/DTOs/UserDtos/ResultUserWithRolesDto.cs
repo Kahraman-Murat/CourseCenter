0ce52206772c16e744e7541166c318c5f7f03dd8
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-12-27 07:41:00 +0100

    feat(DTO/WebUI): add ResultUserWithRolesDto for user-role data
    - Introduced `ResultUserWithRolesDto` in the DTO and WebUI layers to support fetching user lists with associated roles.
    - Facilitates structured data representation for user-role mapping in the WebUI.

---
+++ CourseCenter.DTO/DTOs/UserDtos/ResultUserWithRolesDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.DTO.DTOs.UserDtos
+{
+    public class ResultUserWithRolesDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
