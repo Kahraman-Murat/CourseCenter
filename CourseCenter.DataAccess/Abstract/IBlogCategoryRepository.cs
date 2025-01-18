00f39135303fb167f1ca4ed599456f2eebb0ef7a
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-01-18 19:48:31 +0100

    feat(DataAccess): add BlogCategoryRepository with GetCategoriesWithBlogs method
    - Created `BlogCategoryRepository` in the DataAccess layer.
    - Added `GetCategoriesWithBlogs` method to fetch categories along with their associated blogs.

---
+++ CourseCenter.DataAccess/Abstract/IBlogCategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using CourseCenter.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.DataAccess.Abstract
+{
+    public interface IBlogCategoryRepository: IRepository<BlogCategory>
+    {
+        List<BlogCategory> GetCategoriesWithBlogs();
+    }
+}
---
+++ CourseCenter.DataAccess/Concrete/BlogCategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using CourseCenter.DataAccess.Abstract;
+using CourseCenter.DataAccess.Context;
+using CourseCenter.DataAccess.Repositories;
+using CourseCenter.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.DataAccess.Concrete
+{
+    public class BlogCategoryRepository : GenericRepository<BlogCategory>, IBlogCategoryRepository
+    {
+        public BlogCategoryRepository(CourseCenterContext context) : base(context)
+        {
+        }
+
+        public List<BlogCategory> GetCategoriesWithBlogs()
+        {
+            return _context.BlogCategories.Include(x => x.Blogs).ToList();
+        }
+    }
+}
