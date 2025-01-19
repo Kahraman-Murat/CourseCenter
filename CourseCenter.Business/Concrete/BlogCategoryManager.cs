54ed5019c436f6bc0001e90e5db131d74ed28f96
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-01-19 20:46:21 +0100

    feat(Business): add IBlogCategoryService and BlogCategoryManager
    - Created `IBlogCategoryService` interface to define business logic for blog categories.
    - Implemented `BlogCategoryManager` to handle operations related to blog categories.

---
+++ CourseCenter.Business/Concrete/BlogCategoryManager.cs
@@ -0,0 +1,25 @@
+﻿using CourseCenter.Business.Abstract;
+using CourseCenter.DataAccess.Abstract;
+using CourseCenter.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.Business.Concrete
+{
+    public class BlogCategoryManager : GenericManager<BlogCategory>, IBlogCategoryService
+    {
+        private readonly IBlogCategoryRepository _blogCategoryRepository;
+        public BlogCategoryManager(IRepository<BlogCategory> _repository, IBlogCategoryRepository blogCategoryRepository) : base(_repository)
+        {
+            _blogCategoryRepository = blogCategoryRepository;
+        }
+
+        public List<BlogCategory> TGetCategoriesWithBlogs()
+        {
+            return _blogCategoryRepository.GetCategoriesWithBlogs();
+        }
+    }
+}
