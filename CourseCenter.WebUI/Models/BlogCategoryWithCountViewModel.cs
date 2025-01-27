59a32d8e8bac2ff17dcc3036b2ee68a2cc6c0f1f
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-01-27 16:23:22 +0100

    feat(WebUI): add BlogCategoryWithCountViewModel for BlogCategoryListComponent
    - Introduced `BlogCategoryWithCountViewModel` to enhance the functionality of `BlogCategoryListComponent` on the Blog page.
    - Enables displaying category information along with blog counts for better user experience.

---
+++ CourseCenter.WebUI/Models/BlogCategoryWithCountViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseCenter.WebUI.Models
+{
+    public class BlogCategoryWithCountViewModel
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int BlogCount { get; set; }
+    }
+}
