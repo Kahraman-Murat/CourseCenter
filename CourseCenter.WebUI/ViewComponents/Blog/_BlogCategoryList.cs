a11f3329adaf028b66b0c1ccf160bf202786bf66
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-01-28 20:02:56 +0100

    feat(WebUI): create BlogCategoryListComponent for Blog page
    - Added `BlogCategoryListComponent` to the Blog page for displaying blog categories.
    - Integrated category listing to enhance navigation and filtering capabilities for users.

---
+++ CourseCenter.WebUI/ViewComponents/Blog/_BlogCategoryList.cs
@@ -0,0 +1,27 @@
+﻿using CourseCenter.WebUI.DTOs.BlogCategoryDtos;
+using CourseCenter.WebUI.Helpers;
+using CourseCenter.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Blog
+{
+    public class _BlogCategoryList(IHttpClientService _httpClientService) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<ResultBlogCategoryDto> categorList = await _httpClientService.SendRequestAsync<string, List<ResultBlogCategoryDto>>(HttpMethod.Get, "blogCategories", default);
+
+            var blogCategories = (from blogCategory in categorList
+                                  select new BlogCategoryWithCountViewModel
+                                  {
+                                      CategoryId = blogCategory.Id,
+                                      CategoryName = blogCategory.Name,
+                                      BlogCount = blogCategory.Blogs.Count
+                                  }).ToList();
+
+            return View(blogCategories);
+        }
+    }
+
+
+}
