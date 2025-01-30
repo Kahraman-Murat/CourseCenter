5f8113697ff5dc11ce1953086a37032e3dd264ff
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-01-30 11:44:06 +0100

    feat(WebUI): create BlogRecentPostComponent for Blog page
    - Added `BlogRecentPostComponent` to display recent blog posts.
    - Enhanced the Blog page by providing users with quick access to the latest posts.

---
+++ CourseCenter.WebUI/ViewComponents/Blog/_BlogRecentPost.cs
@@ -0,0 +1,16 @@
+﻿using CourseCenter.WebUI.DTOs.BlogDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Blog
+{
+    public class _BlogRecentPost(IHttpClientService _httpClientService) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<ResultBlogDto> blogs = await _httpClientService.SendRequestAsync<string, List<ResultBlogDto>>(HttpMethod.Get, "blogs/GetLast4Blogs", default);
+
+            return View(blogs);
+        }
+    }
+}
