5c24058b6bcd049b5c0a8f883042fec7653b2f10
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-02-01 19:10:56 +0100

    feat(WebUI): add BlogSubscribeComponent for Blog page
    - Created `BlogSubscribeComponent` to allow users to subscribe to blog updates.
    - Improved user engagement by enabling subscription functionality on the Blog page.

---
+++ CourseCenter.WebUI/ViewComponents/Blog/_BlogSubscribeComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Blog
+{
+    public class _BlogSubscribeComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
