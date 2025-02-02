b547e2e4551668db6adb9ea5b8d6d4957fc37a86
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-02-02 18:09:07 +0100

    feat(WebUI): add BlogListComponent for Blog page
    - Created `BlogListComponent` to display a list of blogs on the Blog page.
    - Improved content organization and user experience.

---
+++ CourseCenter.WebUI/ViewComponents/Blog/_BlogListComponent.cs
@@ -0,0 +1,12 @@
+﻿using CourseCenter.WebUI.DTOs.BlogDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Blog
+{
+    public class _BlogListComponent(IHttpClientService _httpClientService) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() =>
+            View(await _httpClientService.SendRequestAsync<string, List<ResultBlogDto>>(HttpMethod.Get, "blogs", default));
+    }
+}
