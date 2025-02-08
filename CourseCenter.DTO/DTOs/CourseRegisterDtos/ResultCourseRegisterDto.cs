d2027a652f0c5ccfdd916119bdb65fe58bd59c8e
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-02-08 19:58:03 +0100

    feat(DTOs): add DTOs for CourseRegister operations
    - Created `CreateCourseRegisterDto`, `UpdateCourseRegisterDto`, and `ResultCourseRegisterDto`.
    - Standardized data transfer for course registration processes.

---
+++ CourseCenter.DTO/DTOs/CourseRegisterDtos/ResultCourseRegisterDto.cs
@@ -0,0 +1,21 @@
+﻿using CourseCenter.Entity.Entities.Identity;
+using CourseCenter.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseCenter.DTO.DTOs.UserDtos;
+using CourseCenter.DTO.DTOs.CourseDtos;
+
+namespace CourseCenter.DTO.DTOs.CourseRegisterDtos
+{
+    public class ResultCourseRegisterDto
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public ResultUserDto Student { get; set; }
+        public int CourseId { get; set; }
+        public ResultCourseDto Course { get; set; }
+    }
+}
