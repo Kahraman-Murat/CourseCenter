cc7415040cebc91c2217b1269ee05d9a00363c25
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-02-09 13:08:56 +0100

    feat(DataAccess): create CourseRegisterRepository
    - Added `CourseRegisterRepository` to manage course registration data operations.
    - Provides data access functionality for course registration processes.

---
+++ CourseCenter.DataAccess/Abstract/ICourseRegisterRepository.cs
@@ -0,0 +1,16 @@
+﻿using CourseCenter.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.DataAccess.Abstract
+{
+    public interface ICourseRegisterRepository : IRepository<CourseRegister>
+    {
+        List<CourseRegister> GetRegistersWithCourseUndCategory(Expression<Func<CourseRegister, bool>> filter);
+
+    }
+}
---
+++ CourseCenter.DataAccess/Concrete/CourseRegisterRepository.cs
@@ -0,0 +1,26 @@
+﻿using CourseCenter.DataAccess.Abstract;
+using CourseCenter.DataAccess.Context;
+using CourseCenter.DataAccess.Repositories;
+using CourseCenter.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.DataAccess.Concrete
+{
+    public class CourseRegisterRepository : GenericRepository<CourseRegister>, ICourseRegisterRepository
+    {
+        public CourseRegisterRepository(CourseCenterContext _context) : base(_context)
+        {
+        }
+
+        public List<CourseRegister> GetRegistersWithCourseUndCategory(Expression<Func<CourseRegister, bool>> filter)
+        {
+            return _context.CourseRegisters.Where(filter).Include(x => x.Course).ThenInclude(c => c.CourseCategory).Include(t => t.Course.Teacher).ToList();
+        }
+    }
+}
