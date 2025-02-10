e8976993cd8cadd833ce82a5680e3d31a6f67e73
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-02-10 17:17:27 +0100

    feat(Business): add CourseRegisterManager with TGetRegistersWithCourseUndCategory method
    - Created `CourseRegisterManager` to handle course registration business logic.
    - Implemented `TGetRegistersWithCourseUndCategory` method to retrieve course registrations with associated course and category details.

---
+++ CourseCenter.Business/Abstract/ICourseRegisterService.cs
@@ -0,0 +1,16 @@
+﻿using CourseCenter.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.Business.Abstract
+{
+    public interface ICourseRegisterService : IGenericService<CourseRegister>
+    {
+        List<CourseRegister> TGetRegistersWithCourseUndCategory(Expression<Func<CourseRegister,bool>> filter);
+
+    }
+}
---
+++ CourseCenter.Business/Concrete/CourseRegisterManager.cs
@@ -0,0 +1,28 @@
+﻿using CourseCenter.Business.Abstract;
+using CourseCenter.DataAccess.Abstract;
+using CourseCenter.DataAccess.Concrete;
+using CourseCenter.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.Business.Concrete
+{
+    public class CourseRegisterManager : GenericManager<CourseRegister>, ICourseRegisterService
+    {
+        private readonly ICourseRegisterRepository _courseRegisterRepository;
+
+        public CourseRegisterManager(IRepository<CourseRegister> _repository, ICourseRegisterRepository courseRegisterRepository) : base(_repository)
+        {
+            _courseRegisterRepository = courseRegisterRepository;
+        }
+
+        public List<CourseRegister> TGetRegistersWithCourseUndCategory(Expression<Func<CourseRegister, bool>> filter)
+        {
+            return _courseRegisterRepository.GetRegistersWithCourseUndCategory(filter);
+        }
+    }
+}
