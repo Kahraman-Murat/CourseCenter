e0c5b97c102035894445c58041eefe0213b78538
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-02-11 12:21:07 +0100

    feat(API): add CourseRegistersController and CourseRegisterMapping
    - Created `CourseRegistersController` to manage course registrations.
    - Added `CourseRegisterMapping` to handle DTO-to-entity mappings for course registration operations.

---
+++ CourseCenter.API/Controllers/CourseRegistersController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using CourseCenter.Business.Abstract;
+using CourseCenter.DTO.DTOs.CourseRegisterDtos;
+using CourseCenter.Entity.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.API.Controllers
+{
+
+    [Authorize(Roles = "Admin,Content-Manager,Student")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseRegistersController(ICourseRegisterService _courseRegisterService, IMapper _mapper) : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var datas = _courseRegisterService.TGetList();
+            var mappedCourses = _mapper.Map<List<ResultCourseRegisterDto>>(datas);
+            return Ok(mappedCourses);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var course = _courseRegisterService.TGetById(id);
+            var mappedCourse = _mapper.Map<ResultCourseRegisterDto>(course);
+            return Ok(mappedCourse);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("GetCourseByStudentId")]
+        public IActionResult GetCourseByStudentId()
+        {
+            var claim = User.Claims.FirstOrDefault(c => c.Type.Contains("nameidentifier"));
+            string claimValue = claim == null ? "0" : claim.Value;
+
+            //var courses = _courseRegisterService.TGetFilteredList(x => x.StudentId.ToString() == "10");//claimValue
+            var courses = _courseRegisterService.TGetRegistersWithCourseUndCategory(x => x.StudentId.ToString() == claimValue);
+            var mappedCourses = _mapper.Map<List<ResultCourseRegisterDto>>(courses);
+            return Ok(mappedCourses);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _courseRegisterService.TDelete(id);
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCourseRegisterDto createCourseRegisterDto)
+        {
+            if (createCourseRegisterDto.StudentId == 0)
+            {
+                var claim = User.Claims.FirstOrDefault(c => c.Type.Contains("nameidentifier"));
+                string claimValue = claim == null ? "0" : claim.Value;
+                createCourseRegisterDto.StudentId = Int32.Parse(claimValue);
+            }
+            var newData = _mapper.Map<CourseRegister>(createCourseRegisterDto);
+            _courseRegisterService.TCreate(newData);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateCourseRegisterDto updateCourseRegisterDto)
+        {
+            var newData = _mapper.Map<CourseRegister>(updateCourseRegisterDto);
+            _courseRegisterService.TUpdate(newData);
+            return Ok();
+        }
+
+    }
+}
---
+++ CourseCenter.API/Mapping/CourseRegisterMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CourseCenter.DTO.DTOs.CourseRegisterDtos;
+using CourseCenter.Entity.Entities;
+
+namespace CourseCenter.API.Mapping
+{
+    public class CourseRegisterMapping : Profile
+    {
+        public CourseRegisterMapping()
+        {
+            CreateMap<CreateCourseRegisterDto, CourseRegister>().ReverseMap();
+            CreateMap<UpdateCourseRegisterDto, CourseRegister>().ReverseMap();
+            CreateMap<ResultCourseRegisterDto, CourseRegister>().ReverseMap();
+        }
+    }
+}
