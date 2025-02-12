2646b19df3971be1fb0934486fe095f57208e0f8
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-02-12 19:12:18 +0100

    feat(WebUI, DTOs): add CourseRegister DTOs for Create, Update, and Result
    - Introduced `CreateCourseRegisterDto`, `UpdateCourseRegisterDto`, and `ResultCourseRegisterDto`.
    - Enables structured data handling for course registration processes.

---
+++ CourseCenter.WebUI/DTOs/CourseRegisterDtos/ResultCourseRegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseCenter.WebUI.DTOs.UserDtos;
+using CourseCenter.WebUI.DTOs.CourseDtos;
+
+namespace CourseCenter.WebUI.DTOs.CourseRegisterDtos
+{
+    public class ResultCourseRegisterDto
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public ResultUserDto Student { get; set; }
+        public int CourseId { get; set; }
+        public ResultCourseDto Course { get; set; }
+    }
+}
