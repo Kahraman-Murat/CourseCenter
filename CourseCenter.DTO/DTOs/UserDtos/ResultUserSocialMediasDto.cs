53ff3af2d7de9767c89192075f6de8acaf0ee87c
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-02-21 12:02:41 +0100

    feat(DTOs): add ResultUserSocialMediasDto
    - Created `ResultUserSocialMediasDto` to structure user social media data.
    - Enables consistent data transfer for user social media details across layers.

---
+++ CourseCenter.DTO/DTOs/UserDtos/ResultUserSocialMediasDto.cs
@@ -0,0 +1,20 @@
+﻿using CourseCenter.DTO.DTOs.TeacherSocialMediaDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCenter.DTO.DTOs.UserDtos
+{
+    public class ResultUserSocialMediasDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string? ImageUrl { get; set; }
+        public List<ResultTeacherSocialMediaDto> TeacherSocialMedias { get; set; }
+
+    }
+}
