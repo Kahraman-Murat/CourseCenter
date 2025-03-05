87560a6589948b084f3c6d7d92fa0e5de489554c
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-05 22:05:58 +0100

    feat(WebUI): add Validators for CourseRegister Create & Update
    - Implemented validation rules for `CourseRegister` create and update operations.
    - Ensured data integrity and consistency within the `WebUI` layer.

---
+++ CourseCenter.WebUI/Validators/CreateCourseRegisterValidator.cs
@@ -0,0 +1,15 @@
+﻿using CourseCenter.WebUI.DTOs.CourseRegisterDtos;
+using FluentValidation;
+
+namespace CourseCenter.WebUI.Validators
+{
+    public class CreateCourseRegisterValidator : AbstractValidator<CreateCourseRegisterDto>
+    {
+        public CreateCourseRegisterValidator()
+        {
+            //RuleFor(x => x.StudentId).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.CourseId).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+        }
+    }
+}
---
+++ CourseCenter.WebUI/Validators/UpdateCourseRegisterValidator.cs
@@ -0,0 +1,15 @@
+﻿using CourseCenter.WebUI.DTOs.CourseRegisterDtos;
+using FluentValidation;
+
+namespace CourseCenter.WebUI.Validators
+{
+    public class UpdateCourseRegisterValidator : AbstractValidator<UpdateCourseRegisterDto>
+    {
+        public UpdateCourseRegisterValidator()
+        {
+            RuleFor(x => x.StudentId).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.CourseId).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+        }
+    }
+}
