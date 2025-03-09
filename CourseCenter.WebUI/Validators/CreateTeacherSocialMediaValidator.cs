78ff316cd9f51678f405a3b3e7738bf376a08c6f
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-09 13:25:20 +0100

    feat(WebUI): add Create and Update Validators for TeacherSocialMedia
    - Implemented validation rules for `TeacherSocialMedia` create and update operations.
    - Ensured data integrity and consistency in TeacherSocialMedia forms.

---
+++ CourseCenter.WebUI/Validators/CreateTeacherSocialMediaValidator.cs
@@ -0,0 +1,18 @@
+﻿using CourseCenter.WebUI.DTOs.TeacherSocialMediaDtos;
+using FluentValidation;
+
+namespace CourseCenter.WebUI.Validators
+{
+    public class CreateTeacherSocialMediaValidator : AbstractValidator<CreateTeacherSocialMediaDto>
+    {
+        public CreateTeacherSocialMediaValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.Icon).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.Url).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+        }
+    }
+}
---
+++ CourseCenter.WebUI/Validators/UpdateTeacherSocialMediaValidator.cs
@@ -0,0 +1,18 @@
+﻿using CourseCenter.WebUI.DTOs.TeacherSocialMediaDtos;
+using FluentValidation;
+
+namespace CourseCenter.WebUI.Validators
+{
+    public class UpdateTeacherSocialMediaValidator : AbstractValidator<UpdateTeacherSocialMediaDto>
+    {
+        public UpdateTeacherSocialMediaValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.Icon).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.Url).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+        }
+    }
+}
