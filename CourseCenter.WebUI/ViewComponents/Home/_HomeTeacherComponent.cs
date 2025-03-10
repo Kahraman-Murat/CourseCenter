0d22b61a952dc655bd89e3214132ae60e63debbf
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-10 10:47:15 +0100

    feat(WebUI): add _HomeTeacherComponent for Home Index page
    - Created `_HomeTeacherComponent` to display teacher-related content on the Home Index page.
    - Enhanced modularity and maintainability of the Home page components.

---
+++ CourseCenter.WebUI/ViewComponents/Home/_HomeTeacherComponent.cs
@@ -0,0 +1,12 @@
+﻿using CourseCenter.WebUI.DTOs.UserDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Home
+{
+    public class _HomeTeacherComponent(IHttpClientService _httpClientService) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() =>
+            View(await _httpClientService.SendRequestAsync<string, List<ResultUserSocialMediasDto>>(HttpMethod.Get, "users/GetLast4Teachers", default));
+    }
+}
