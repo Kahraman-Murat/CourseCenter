7de2f74f7642701a960cf0b3a301f94a082c9d9b
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-11 12:39:57 +0100

    feat(WebUI): add CourseController and Index view for Course page
    - Created `CourseController` to manage course-related operations in WebUI.
    - Added `Index` view to display courses.

---
+++ CourseCenter.WebUI/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using CourseCenter.WebUI.DTOs.CourseCategoryDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.Controllers
+{
+    public class CourseController(IHttpClientService _httpClientService) : Controller
+    {
+
+        [Route("Course/Index/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            ViewBag.Category = id;
+
+            List<ResultCourseCategoryDto> response = await _httpClientService.SendRequestAsync<string, List<ResultCourseCategoryDto>>(HttpMethod.Get, $"CourseCategories/GetCategoriesWithCoursesUndTeacherById/{id}", default);
+
+            return View(response); 
+        }
+    }
+}
