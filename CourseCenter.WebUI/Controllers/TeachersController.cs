48df527dd6bd173dc8fe091dc7677e4dba5e8430
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-14 12:55:12 +0100

    feat(WebUI): add TeachersController and Index view for Teacher page
    - Created `TeachersController` to manage teacher-related operations in WebUI.
    - Added `Index` view to display teachers.

---
+++ CourseCenter.WebUI/Controllers/TeachersController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CourseCenter.WebUI.DTOs.UserDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.Controllers
+{
+    public class TeachersController(IHttpClientService _httpClientService) : Controller
+    {
+        [Route("Teachers/Index/{page}")]
+        public async Task<IActionResult> Index(int page)
+        {
+            List<ResultUserSocialMediasDto> response = await _httpClientService.SendRequestAsync<string, List<ResultUserSocialMediasDto>>(HttpMethod.Get, $"Users/GetTeachersWithSocialMedia/{page}", default);
+
+            return View(response);
+        }
+    }
+}
