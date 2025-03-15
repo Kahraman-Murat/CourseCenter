cc88223793f259fee7eaaaedfa6d6d57f78808fb
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-15 13:25:57 +0100

    feat(WebUI): add _UILayoutFooterSubscribeComponent for UILayout
    - Created `_UILayoutFooterSubscribeComponent` to handle subscription functionality in the footer.
    - Enhanced UI modularity and maintainability.

---
+++ CourseCenter.WebUI/ViewComponents/UILayout/_UILayoutFooterSubscribeComponent.cs
@@ -0,0 +1,15 @@
+﻿
+using CourseCenter.WebUI.DTOs.ContactDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.UILayout
+{
+    public class _UILayoutFooterSubscribeComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
