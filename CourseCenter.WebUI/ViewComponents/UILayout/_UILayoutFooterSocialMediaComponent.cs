02695108319718deaf36a03bb6ef3786bd6e983c
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-16 14:53:34 +0100

    feat(WebUI): add _UILayoutFooterSocialMediaComponent and update UILayout
    - Created `_UILayoutFooterSocialMediaComponent` for displaying social media links in the footer.
    - Updated `UILayout` to integrate the new component for better modularity and structure.

---
+++ CourseCenter.WebUI/ViewComponents/UILayout/_UILayoutFooterSocialMediaComponent.cs
@@ -0,0 +1,12 @@
+﻿using CourseCenter.WebUI.DTOs.SocialMediaDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.UILayout
+{
+    public class _UILayoutFooterSocialMediaComponent(IHttpClientService _httpClientService) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() =>
+            View(await _httpClientService.SendRequestAsync<string, List<ResultSocialMediaDto>>(HttpMethod.Get, "SocialMedias", default));
+    }
+}
