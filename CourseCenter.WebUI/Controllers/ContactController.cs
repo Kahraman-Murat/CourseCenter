bd7cb1aa78f16e28827b223c9648027a747ef74b
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-20 13:45:53 +0100

    feat(WebUI): add ContactController and Index View for Contact page
    - Created ContactController to handle contact page requests.
    - Developed Index View for displaying contact information and forms.

---
+++ CourseCenter.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,41 @@
+﻿using CourseCenter.WebUI.DTOs.ContactDtos;
+using CourseCenter.WebUI.DTOs.MessageDtos;
+using CourseCenter.WebUI.DTOs.SubscriberDtos;
+using CourseCenter.WebUI.Helpers;
+using CourseCenter.WebUI.Validators;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.Controllers
+{
+    public class ContactController(IHttpClientService _httpClientService) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var map = await _httpClientService.SendRequestAsync<string, List<ResultContactDto>>(HttpMethod.Get, "Contacts", default);
+            ViewBag.Map = map.Select(x => x.MapUrl).FirstOrDefault();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage([FromBody] CreateMessageDto createMessageDto)
+        {
+            createMessageDto.EmailDate = DateTime.Now;
+
+            var validator = new CreateMessageValidator();
+            var result = await validator.ValidateAsync(createMessageDto);
+            if (!result.IsValid)
+            {
+                var errors = "";
+                ModelState.Clear();
+                foreach (var x in result.Errors)
+                    errors += x.ErrorMessage + " <br/> ";
+
+                return Ok(new ResponseMessageDto { Success = false, Message = errors });
+            }
+
+            ResponseMessageDto response = await _httpClientService.SendRequestAsync<CreateMessageDto, ResponseMessageDto>(HttpMethod.Post, "Messages", createMessageDto);
+
+            return Ok(response);
+        }
+    }
+}
