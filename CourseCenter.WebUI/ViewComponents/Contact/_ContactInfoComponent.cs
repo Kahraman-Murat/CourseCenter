670848c67395a0e546a179f2dbc7f6706b0aee55
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-23 09:18:48 +0100

    feat(WebUI): add _ContactInfoComponent for Contact page
    - Created _ContactInfoComponent to display contact details on the Contact page.

---
+++ CourseCenter.WebUI/ViewComponents/Contact/_ContactInfoComponent.cs
@@ -0,0 +1,15 @@
+﻿using CourseCenter.WebUI.DTOs.ContactDtos;
+using CourseCenter.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.WebUI.ViewComponents.Contact
+{
+    public class _ContactInfoComponent(IHttpClientService _httpClientService) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var datas = await _httpClientService.SendRequestAsync<string, List<ResultContactDto>>(HttpMethod.Get, "Contacts", default);
+            return View(datas);
+        }
+    }
+}
