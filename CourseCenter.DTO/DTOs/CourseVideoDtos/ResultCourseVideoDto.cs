1c1c76b4b8ccc3c1f52e42cf3e55535538fac490
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-27 15:11:13 +0100

    feat(DTOs): add CourseVideo Create, Update, and Result DTOs
    - Created CourseVideoCreateDto for adding new course videos.
    - Created CourseVideoUpdateDto for updating existing course videos.
    - Created CourseVideoResultDto for returning course video data.

---
+++ CourseCenter.DTO/DTOs/CourseVideoDtos/ResultCourseVideoDto.cs
@@ -0,0 +1,16 @@
+﻿using CourseCenter.DTO.DTOs.CourseDtos;
+
+namespace CourseCenter.DTO.DTOs.CourseVideoDtos
+{
+    public class ResultCourseVideoDto
+    {
+        public int Id { get; set; }
+        public int VideoNumber { get; set; }
+        public string VideoTitle { get; set; }
+        public string VideoContent { get; set; }
+        public string VideoUrl { get; set; }
+
+        public int CourseId { get; set; }
+        public ResultCourseDto Course { get; set; }
+    }
+}
---
+++ CourseCenter.DTO/DTOs/CourseVideoDtos/UpdateCourseVideoDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CourseCenter.DTO.DTOs.CourseVideoDtos
+{
+    public class UpdateCourseVideoDto
+    {
+        public int Id { get; set; }
+        public int VideoNumber { get; set; }
+        public string VideoTitle { get; set; }
+        public string VideoContent { get; set; }
+        public string VideoUrl { get; set; }
+
+        public int CourseId { get; set; }
+    }
+}
