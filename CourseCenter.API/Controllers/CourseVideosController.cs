20ec5ddb07118b0d125c95add232dbe53e12f253
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-03-29 18:40:23 +0100

    feat(API): add CourseVideosController and CourseVideoMapping
    - Created CourseVideosController to manage course video operations.
    - Added CourseVideoMapping for DTO to entity mappings.

---
+++ CourseCenter.API/Controllers/CourseVideosController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using CourseCenter.Business.Abstract;
+using CourseCenter.DTO.DTOs.CourseDtos;
+using CourseCenter.DTO.DTOs.CourseVideoDtos;
+using CourseCenter.Entity.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseCenter.API.Controllers
+{
+    [Authorize(Roles = "Admin,Teacher,Student,Content-Manager")]
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CourseVideosController(IGenericService<CourseVideo> _courseVideoService, IMapper _mapper) : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var datas = _courseVideoService.TGetList();
+            return Ok(datas);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var data = _courseVideoService.TGetById(id);
+            return Ok(data);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _courseVideoService.TDelete(id);
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCourseVideoDto createCourseVideoDto)
+        {
+            var newData = _mapper.Map<CourseVideo>(createCourseVideoDto);
+            _courseVideoService.TCreate(newData);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateCourseVideoDto updateCourseVideoDto)
+        {
+            var newData = _mapper.Map<CourseVideo>(updateCourseVideoDto);
+            _courseVideoService.TUpdate(newData);
+            return Ok();
+        }
+
+    }
+}
---
+++ CourseCenter.API/Mapping/CourseVideoMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CourseCenter.DTO.DTOs.CourseVideoDtos;
+using CourseCenter.Entity.Entities;
+
+namespace CourseCenter.API.Mapping
+{
+    public class CourseVideoMapping : Profile
+    {
+        public CourseVideoMapping()
+        {
+            CreateMap<CreateCourseVideoDto, CourseVideo>().ReverseMap();
+            CreateMap<UpdateCourseVideoDto, CourseVideo>().ReverseMap();
+            CreateMap<ResultCourseVideoDto, CourseVideo>().ReverseMap();
+        }
+    }
+}
