6447b8caddf5c09f214989cbf7b91f86d6a91903
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-04-02 23:21:24 +0200

    feat(WebUI): add CourseVideo Create and Update Validators
    - Implemented validation rules for CourseVideo creation and update processes.
    - Ensured data integrity and consistency for CourseVideo inputs.

---
+++ CourseCenter.WebUI/Validators/CreateCourseVideoValidator.cs
@@ -0,0 +1,21 @@
+﻿using CourseCenter.WebUI.DTOs.CourseVideoDtos;
+using FluentValidation;
+
+namespace CourseCenter.WebUI.Validators
+{
+    public class CreateCourseVideoValidator : AbstractValidator<CreateCourseVideoDto>
+    {
+        public CreateCourseVideoValidator()
+        {
+            RuleFor(x => x.VideoNumber).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.VideoNumber).GreaterThan(0).WithMessage("Bu alan Sıfır dan büyük olmalıdır.");
+
+            RuleFor(x => x.VideoTitle).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.VideoContent).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.VideoUrl).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+        }
+    }
+}
---
+++ CourseCenter.WebUI/Validators/UpdateCourseVideoValıdator.cs
@@ -0,0 +1,21 @@
+﻿using CourseCenter.WebUI.DTOs.CourseVideoDtos;
+using FluentValidation;
+
+namespace CourseCenter.WebUI.Validators
+{
+    public class UpdateCourseVideoValıdator : AbstractValidator<UpdateCourseVideoDto>
+    {
+        public UpdateCourseVideoValıdator()
+        {
+            RuleFor(x => x.VideoNumber).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.VideoNumber).GreaterThan(0).WithMessage("Bu alan Sıfır dan büyük olmalıdır.");
+
+            RuleFor(x => x.VideoTitle).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.VideoContent).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+
+            RuleFor(x => x.VideoUrl).NotEmpty().WithMessage("Bu alan bos birakilamaz.");
+        }
+    }
+}
