7b149931006ece3490d7b5e5618b2d7e58dff452
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-05-08 15:15:58 +0200

    feat(api): added UpdateUserValidator for validating user update requests in UsersController

---
+++ CourseCenter.API/Validators/UpdateUserValidator.cs
@@ -0,0 +1,22 @@
+﻿using CourseCenter.DTO.DTOs.UserDtos;
+using FluentValidation;
+
+namespace CourseCenter.API.Validators
+{
+    public class UpdateUserValidator : AbstractValidator<UpdateUserDto>
+    {
+        public UpdateUserValidator()
+        {
+            RuleFor(x => x.FullName)
+                .NotEmpty().WithMessage("Ad Soyad alanı boş bırakılamaz.");
+
+            RuleFor(x => x.UserName)
+                .NotEmpty().WithMessage("Kullanıcı Adı alanı boş bırakılamaz.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email Adresi alanı boş bırakılamaz.")
+                .EmailAddress().WithMessage("Geçerli bir Email Adresi giriniz.");
+
+        }
+    }
+}
