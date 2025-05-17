173538d46828c02afa95e831c5e73583c3be0ea8
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-05-17 13:25:21 +0200

    feat(dto): add ImageUrl, Base64Image, and ImageFileName fields to UpdateUserDto

--- CourseCenter.DTO/DTOs/UserDtos/UpdateUserDto.cs
+++ CourseCenter.DTO/DTOs/UserDtos/UpdateUserDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,8 @@ namespace CourseCenter.DTO.DTOs.UserDtos
         public string FullName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
+        public string? ImageUrl { get; set; }
+        public string Base64Image { get; set; }
+        public string ImageFileName { get; set; }
     }
 }
