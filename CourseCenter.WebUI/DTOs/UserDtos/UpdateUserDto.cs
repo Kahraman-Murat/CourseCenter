db39309623573357da378b7cb018c3caf2b75e53
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2025-05-18 11:25:17 +0200

    feat(webui): add ImageUrl, Base64Image, and ImageFileName fields to UpdateUserDto

--- CourseCenter.WebUI/DTOs/UserDtos/UpdateUserDto.cs
+++ CourseCenter.WebUI/DTOs/UserDtos/UpdateUserDto.cs
@@ -6,6 +6,9 @@
         public string FullName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
+        public string? ImageUrl { get; set; }
+        public string Base64Image { get; set; }
+        public string ImageFileName { get; set; }
 
     }
 }
